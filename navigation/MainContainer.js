b867fbcc88b4f1a0a3642dbf31c5c65c29bd4a36
Carlo <carlouxljlj@gmail.com>
2022-11-07 19:07:04 -0600

    Migration to stack navigation nested tab navigator
    Solved "detailsName" to "rankingName"

--- navigation/MainContainer.js
+++ navigation/MainContainer.js
@@ -2,6 +2,8 @@ import * as React from "react";
 import { Platform, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from '@react-navigation/stack';
+import { BlurView } from 'expo-blur';
 import Ionicons from "react-native-vector-icons/Ionicons";
 
 // Screens
@@ -13,27 +15,52 @@ import SignInScreen from "./screens/SignInScreen";
 import SignUpScreen from "./screens/SignUpScreen";
 import VolunteerFormScreen from "./screens/VolunteerFormScreen";
 import AllyFormScreen from "./screens/AllyFormScreen";
+import DetailsScreen from "./screens/DetailsScreen";
 
 //Screen namesrr
 const homeName = "Home";
 const detailsName = "Details";
 const profileName = "Profile";
+const rankingName = "Ranking";
 
+const RootStack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
-function MainContainer() {
+function Root() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
+    <Tab.Navigator
         initialRouteName={homeName}
         screenOptions={({ route }) => ({
+          headerShown: false,
+          tabBarActiveTintColor: "#FFE500",
+          tabBarInactiveTintColor: "#FFF2CB",
+          tabBarActiveBackgroundColor: "#F12749",
+          tabBarInactiveBackgroundColor: "#F12749",
+          tabBarStyle:{
+            borderTopLeftRadius: 15,
+            borderTopRightRadius: 15,
+            borderTopWidth: 0,
+            paddingTop: 10,
+            position: "absolute",
+            backgroundColor: "#F12749",
+            elevation: 0,
+          },
+          tabBarBadgeStyle:{
+            padding: 10,
+          },
+          tabBarLabelStyle:{
+            padding: 0,
+            fontSize: 10,
+          },
+
+
           tabBarIcon: ({ focused, color, size }) => {
             let iconName;
             let rn = route.name;
 
             if (rn === homeName) {
               iconName = focused ? "home" : "home-outline";
-            } else if (rn === detailsName) {
+            } else if (rn === rankingName) {
               iconName = focused ? "list" : "list-outline";
             } else if (rn === profileName) {
               iconName = focused ? "person" : "person-outline";
@@ -43,22 +70,24 @@ function MainContainer() {
             return <Ionicons name={iconName} size={size} color={color} />;
           },
         })}
-        tabBarOptions={{
-          activeTintColor: "#FFE500",
-          inactiveTintColor: "#FFF2CB",
-
-          labelStyle: { paddingBottom: 10, fontSize: 10 },
-          style: {
-            backgroundColor: "#F12749",
-            padding: 10,
-            height: styles.height,
-          },
-        }}
       >
         <Tab.Screen name={homeName} component={HomeScreen} />
         <Tab.Screen name={profileName} component={AllyFormScreen} />
-        <Tab.Screen name={detailsName} component={RankingScreen} />
-      </Tab.Navigator>
+        <Tab.Screen name={rankingName} component={RankingScreen} />
+    </Tab.Navigator>
+  );
+}
+
+function MainContainer() {
+  return (
+    <NavigationContainer>
+      <RootStack.Navigator screenOptions={{headerShown: false}}>
+        <RootStack.Screen
+            name="Root"
+            component={Root}
+            screenOptions={{ headerShown: false }}
+        />
+      </RootStack.Navigator>
     </NavigationContainer>
   );
 }
