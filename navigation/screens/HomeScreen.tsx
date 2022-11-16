ab7e574446a99d19d8d137fca14b4a566a0d6fe2
Carlo <carlouxljlj@gmail.com>
2022-11-15 18:59:52 -0600

    Home Screen fetching from firebase

--- navigation/screens/HomeScreen.tsx
+++ navigation/screens/HomeScreen.tsx
@@ -14,6 +14,11 @@ import { Item as ItemType } from "../../components/HomeScreenFeedItem";
 import InformacionIconButton from "../../components/InformacionIconButton";
 import { AuthContext } from "../../context/AuthContext";
 import palette from "../../palette";
+import { collection, getDocs, QuerySnapshot } from "firebase/firestore";
+import { db } from "../../firebase";
+import { useEffect, useState } from "react";
+
+var DATA1 : [News?] = [] ;
 
 const DATA = [
   {
@@ -42,50 +47,131 @@ const DATA = [
   },
 ];
 
+class News {
+  id: number
+  text: string
+  title: string
+  uri: string 
+  constructor (id: number, text: string, title: string, uri: string ) {
+      this.id = id;
+      this.text = text;
+      this.title = title;
+      this.uri = uri;
+  }
+  toString() {
+      return this.id + ', ' + this.text + ', ' + this.title + ', ' + this.uri;
+  }
+}
+
+// Firestore data converter
+const newsConverter = {
+  toFirestore: (news: News) => {
+      return {
+        id: news.id,
+        text: news.text,
+        title: news.title,
+        uri: news.uri
+      };
+  },
+  fromFirestore: (snapshot, options) => {
+      const data = snapshot.data(options);
+      return new News(data.id, data.text, data.title, data.uri);
+  }
+};
+
 export default function HomeScreen({ navigation }) {
+
+  const [data, setData] = useState<[News?]>([]);
+  const [loading, setLoading] = useState(true);
+  
+  const myAsyncFunction = async (): Promise<[News?]> => {
+    const querySnapshot = await getDocs(collection(db, "news").withConverter(newsConverter));
+    const data : [News?] = [];
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      const news : News = new News(doc.get("id"), doc.get("text"), doc.get("title"), doc.get("uri"));
+      data.push(news)
+    })
+    return data
+  }
+
   const renderItem = ({ item }: { item: ItemType }) => (
     <Item id={item.id} title={item.title} text={item.text} uri={item.uri} />
   );
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <View
-        style={{
-          paddingLeft: "10%",
-          alignItems: "center",
-          justifyContent: "space-between",
-          alignContent: "space-between",
-          flexDirection: "row",
-          paddingRight: "10%",
-        }}
-      >
-        <Text style={styles.title}>Hola, Carlo </Text>
-        <InformacionIconButton screen={"Details"} />
-      </View>
-      <View style={styles.storiesContainer}>
-        <View style={{ alignItems: "center" }}>
-          <Text
-            style={{
-              justifyContent: "center",
-              textAlign: "center",
-              alignContent: "center",
-              alignItems: "center",
-              fontSize: 22,
-              fontWeight: "bold",
-              marginBottom: 10,
-            }}
-          >
-            Esto es lo que ha estado haciendo BAMX:
-          </Text>
+  useEffect(() => {
+    setLoading(true);
+    const fetchData = async() => {
+      const data = await myAsyncFunction();
+      setData(data);
+      setLoading(false)
+    }
+    try{
+      fetchData()
+    } catch (error) {
+      console.log(error)
+      setLoading(false)
+    }
+  }, []);
+
+  if (loading) {
+    return (
+      <SafeAreaView style={styles.container}>
+        <View
+          style={{
+            paddingLeft: "10%",
+            alignItems: "center",
+            justifyContent: "space-between",
+            alignContent: "space-between",
+            flexDirection: "row",
+            paddingRight: "10%",
+          }}
+        >
+          <Text style={styles.title}>Loading...</Text>
         </View>
-        <FlatList
-          data={DATA}
-          renderItem={renderItem}
-          keyExtractor={(item) => item.id}
-        />
-      </View>
-    </SafeAreaView>
-  );
+      </SafeAreaView>
+    );
+  } else {
+    return (
+      <SafeAreaView style={styles.container}>
+        <View
+          style={{
+            paddingLeft: "10%",
+            alignItems: "center",
+            justifyContent: "space-between",
+            alignContent: "space-between",
+            flexDirection: "row",
+            paddingRight: "10%",
+          }}
+        >
+          <Text style={styles.title}>Hola, Carlo </Text>
+          <InformacionIconButton screen={"Details"} />
+        </View>
+        <View style={styles.storiesContainer}>
+          <View style={{ alignItems: "center" }}>
+            <Text
+              style={{
+                justifyContent: "center",
+                textAlign: "center",
+                alignContent: "center",
+                alignItems: "center",
+                fontSize: 22,
+                fontWeight: "bold",
+                marginBottom: 10,
+              }}
+            >
+              Esto es lo que ha estado haciendo BAMX:
+            </Text>
+          </View>
+          <FlatList
+            data={data}
+            renderItem={renderItem}
+            keyExtractor={(item) => String(item.id)}
+          />
+        </View>
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
