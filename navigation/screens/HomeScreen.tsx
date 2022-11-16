a28f62c5ce1a6d75b4b36ac61da3afb5368e02f7
Carlo <carlouxljlj@gmail.com>
2022-11-15 19:16:08 -0600

    Deleted unnecessary code

--- navigation/screens/HomeScreen.tsx
+++ navigation/screens/HomeScreen.tsx
@@ -17,36 +17,6 @@ import palette from "../../palette";
 import { collection, getDocs, QuerySnapshot } from "firebase/firestore";
 import { db } from "../../firebase";
 import { useEffect, useState } from "react";
-
-var DATA1 : [News?] = [] ;
-
-const DATA = [
-  {
-    id: "1",
-    title: "Donaciones directas",
-    uri: "https://www.gob.mx/cms/uploads/article/main_image/17642/UNI177015_Richter_2_con-700.jpg",
-    text: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
-  },
-  {
-    id: "2",
-    title: "Un nuevo mundo",
-    uri: "https://us.123rf.com/450wm/niratpix/niratpix1510/niratpix151000172/46843602-nakhon-ratchasima-tailandia-17-de-octubre-un-ni%C3%B1o-no-identificado-en-%C3%A1reas-rurales-est%C3%A1-jugando-para.jpg?ver=6",
-    text: `It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,`,
-  },
-  {
-    id: "3",
-    title: "Logros BAMX - Mayo/22",
-    uri: "https://eacnur.org/files/styles/two_columns_photo_info/public/paragraph/two_columns_foto_info/rf2160921_seeds_for_solutions_c_wouter_elsen._vsf-b_6.jpg?itok=FxG1X8Pb",
-    text: `It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here'`,
-  },
-  {
-    id: "4",
-    title: "Logros BAMX - Mayo/22",
-    uri: "https://espanol.babycenter.com/ims/2015/05/iStock_63184515_wide.jpg",
-    text: `There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.`,
-  },
-];
-
 class News {
   id: number
   text: string
