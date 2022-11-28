419a5d8af0abc640b3747569608ac5a76b3e28e9
Carlo <carlouxljlj@gmail.com>
2022-11-28 12:59:25 -0600

    Update SignUpScreen.tsx

--- navigation/screens/SignUpScreen.tsx
+++ navigation/screens/SignUpScreen.tsx
@@ -70,81 +70,81 @@ export default function SignUpScreen({ navigation }) {
       behavior={Platform.OS === "ios" ? "padding" : "height"}
       style={styles.container}
     >
-    <ScrollView contentContainerStyle={{flexGrow: 1}}
-    keyboardShouldPersistTaps='handled'
-    style={{backgroundColor: palette.background}}
-    >
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        paddingTop: 50,
-        backgroundColor: palette.background,
-      }}
-    >
-      <View
-        style={{ flexDirection: "row", alignItems: "center", marginBottom: 30 }}
+      <ScrollView contentContainerStyle={{flexGrow: 1}}
+      keyboardShouldPersistTaps='handled'
+      style={{backgroundColor: palette.background}}
       >
-        <Image
+        <View
           style={{
-            alignSelf: "flex-start",
-            width: 200,
-            height: 200,
-            marginLeft: -220,
+            flex: 1,
+            alignItems: "center",
+            paddingTop: 50,
+            backgroundColor: palette.background,
           }}
-          source={require("../../assets/icon_signUp.png")}
-        ></Image>
-        <Text style={styles.title}>Crear Cuenta</Text>
-      </View>
+        >
+          <View
+            style={{ flexDirection: "row", alignItems: "center", marginBottom: 30 }}
+          >
+            <Image
+              style={{
+                alignSelf: "flex-start",
+                width: 200,
+                height: 200,
+                marginLeft: -220,
+              }}
+              source={require("../../assets/icon_signUp.png")}
+            ></Image>
+            <Text style={styles.title}>Crear Cuenta</Text>
+          </View>
 
-      {loading && <Text>Creando cuenta...</Text>}
-      <BanInput
-        placeholder="Nombre Completo"
-        onChange={setNameInputValue}
-        value={nameInputValue}
-        containerStyle={{ width: 325, height: 55, marginBottom: 15 }}
-        type="default"
-      ></BanInput>
-      <BanInput
-        placeholder="Usuario"
-        onChange={setUsernameInputValue}
-        value={usenameInputValue}
-        containerStyle={{ width: 325, height: 55, marginBottom: 15 }}
-        type="default"
-      ></BanInput>
-      <BanInput
-        placeholder="Correo"
-        onChange={setMailInputValue}
-        value={mailInputValue}
-        containerStyle={{ width: 325, height: 55, marginBottom: 15 }}
-        type="email-address"
-      ></BanInput>
-      <BanInput
-        placeholder="Contraseña"
-        onChange={setPasswordInputValue}
-        value={passwordInputValue}
-        containerStyle={{ width: 325, height: 55, marginBottom: 40 }}
-        secureTextEntry={true}
-        type="default"
-      ></BanInput>
-      <Button
-        title={"Crear Cuenta"}
-        buttonStyle={styles.button}
-        onPress={handleSubmit}
-        disabled={loading}
-      ></Button>
-      <Text style={{ marginBottom: 20 }}>_____________ o _____________</Text>
-      <Button
-        titleStyle={{ color: palette.black }}
-        title={"Crear cuenta con Google"}
-        buttonStyle={styles.buttonGoogle}
-      ></Button>
-      <Button
-        title={"Crear cuenta con Facebook"}
-        buttonStyle={styles.buttonFacebook}
-      ></Button>
-    </View>
-    </ScrollView>
+          {loading && <Text>Creando cuenta...</Text>}
+          <BanInput
+            placeholder="Nombre Completo"
+            onChange={setNameInputValue}
+            value={nameInputValue}
+            containerStyle={{ width: 325, height: 55, marginBottom: 15 }}
+            type="default"
+          ></BanInput>
+          <BanInput
+            placeholder="Usuario"
+            onChange={setUsernameInputValue}
+            value={usenameInputValue}
+            containerStyle={{ width: 325, height: 55, marginBottom: 15 }}
+            type="default"
+          ></BanInput>
+          <BanInput
+            placeholder="Correo"
+            onChange={setMailInputValue}
+            value={mailInputValue}
+            containerStyle={{ width: 325, height: 55, marginBottom: 15 }}
+            type="email-address"
+          ></BanInput>
+          <BanInput
+            placeholder="Contraseña"
+            onChange={setPasswordInputValue}
+            value={passwordInputValue}
+            containerStyle={{ width: 325, height: 55, marginBottom: 40 }}
+            secureTextEntry={true}
+            type="default"
+          ></BanInput>
+          <Button
+            title={"Crear Cuenta"}
+            buttonStyle={styles.button}
+            onPress={handleSubmit}
+            disabled={loading}
+          ></Button>
+          <Text style={{ marginBottom: 20 }}>_____________ o _____________</Text>
+          <Button
+            titleStyle={{ color: palette.black }}
+            title={"Crear cuenta con Google"}
+            buttonStyle={styles.buttonGoogle}
+          ></Button>
+          <Button
+            title={"Crear cuenta con Facebook"}
+            buttonStyle={styles.buttonFacebook}
+          ></Button>
+        </View>
+      </ScrollView>
     </KeyboardAvoidingView>
   );
 }
