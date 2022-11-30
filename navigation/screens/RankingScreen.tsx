025cd4088550001ddf57369e57ef525c07cbc943
Int-Angel <Padilla.angel88@gmail.com>
2022-11-30 17:36:38 -0600

    Update RankingScreen.tsx

--- navigation/screens/RankingScreen.tsx
+++ navigation/screens/RankingScreen.tsx
@@ -9,176 +9,231 @@ import {
   Switch,
   TouchableOpacity,
   ScrollView,
-  FlatList
+  FlatList,
 } from "react-native";
 import palette from "../../palette";
-import { query, orderBy, limit, collection, getDocs, doc, setDoc,  getDoc, where} from "firebase/firestore";  
-import {auth, db } from "../../firebase";
+import {
+  query,
+  orderBy,
+  limit,
+  collection,
+  getDocs,
+  doc,
+  setDoc,
+  getDoc,
+  where,
+} from "firebase/firestore";
+import { auth, db } from "../../firebase";
 import StatProfile from "../../components/StatProfile";
 import Dashboard from "../../components/Dashboard";
 import ProfileMap from "../../components/ProfileMap";
 import InformacionIconButton from "../../components/InformacionIconButton";
 
-
-const You = ({item}) =>{
+const You = ({ item }) => {
   return item != undefined ? (
-  <View style = {styles.row_with_margin}>
-          <StatProfile item = {{place: item.place, user: "Tú", value:item.donations}}/>
-        </View> )  :
-        (
-          <View style = {styles.row_with_margin}>
-                  
-                </View> ) }
+    <View style={styles.row_with_margin}>
+      <StatProfile
+        item={{ place: item.place, user: "Tú", value: item.donations }}
+      />
+    </View>
+  ) : (
+    <View style={styles.row_with_margin}></View>
+  );
+};
 
-const Item = ({ place, user, value, isYourPosition }) => (
-  place !=3 ? 
-    isYourPosition? ((
-      <You item = {{place: place, donations:value}}/>))
-      
-        :
-  ((
-  <View style = {styles.row_with_margin}>
-          <StatProfile item = {{place: place, user: user != undefined? user : "Anónimo", value:value}}/>
-        </View> ) )
-        : (
-  <View style = {styles.row_with_margin_and_line}>
-          <StatProfile item = {{place: place, user:user != undefined? user : "Anónimo", value:value}}/>
-        </View> )
-);
+const Item = ({ place, user, value, isYourPosition }) =>
+  place != 3 ? (
+    isYourPosition ? (
+      <You item={{ place: place, donations: value }} />
+    ) : (
+      <View style={styles.row_with_margin}>
+        <StatProfile
+          item={{
+            place: place,
+            user: user != undefined ? user : "Anónimo",
+            value: value,
+          }}
+        />
+      </View>
+    )
+  ) : (
+    <View style={styles.row_with_margin_and_line}>
+      <StatProfile
+        item={{
+          place: place,
+          user: user != undefined ? user : "Anónimo",
+          value: value,
+        }}
+      />
+    </View>
+  );
 export default function RankingScreen({ navigation }) {
-
   const [data, setData] = React.useState();
   const [stats, setStats] = React.useState();
 
   const donations = collection(db, "donations");
 
-    const getData = async (parameter) => {
-        const snapshot = await getDocs(query(donations, orderBy(parameter, "desc"), limit(3)))
-        let response = []
-        let idx = 1;
-        let json;
-        snapshot.forEach((doc) => {
-          console.log(doc.id, " => ", doc.data());
-          json = doc.data()
-          json["id"] = doc.id
-          json["place"] = idx;
-          json["value"] = doc.data()[parameter]
-          response.push(json);
-          idx ++;
-          
-        });
-        const yourPosition = await getYourPosition(parameter);
-        console.log("YP L : ", yourPosition.length);
-        
-        if (yourPosition.length > 0){
-          response.push(yourPosition[0]);
-        }
-        
-        console.log(response.length)
-        setData(response)
-    }
-    
-    const getStats = async () => {
-      const stats = await getDoc(doc(donations, "stats"))
-      let response = stats.exists() ? stats.data().total : 0;
-      setStats(response)
-    
-  }
-    const getYourPosition = async (parameter) => {
-      const snapshot = await getDocs(query(donations, where("user", "==", auth["currentUser"]["displayName"]), limit(1)))
-        let response = []
-        let idx = 1;
-        let json;
-        if (!snapshot.empty){
-        snapshot.forEach((doc) => {
-          // doc.data() is never undefined for query doc snapshots
-          console.log(doc.id, " => ", doc.data());
-          json = doc.data()
-          json["id"] = doc.id
-          json["place"] = idx;
-          json["value"] = doc.data()[parameter]
-          json["isYourPosition"] = true
-          response.push(json);
-          idx ++;
-          
-        });
-      }
-      else{
-        await setDoc(doc(donations), {user:auth["currentUser"]["displayName"], referals:0, donations: 0, total:0})
-        const snapshot = await getDocs(query(donations, where("user", "==", auth["currentUser"]["displayName"]), limit(1)))
-        snapshot.forEach((doc) => {
-          // doc.data() is never undefined for query doc snapshots
-          console.log(doc.id, " => ", doc.data());
-          json = doc.data()
-          json["id"] = doc.id
-          json["place"] = idx;
-          json["value"] = doc.data()[parameter]
-          json["isYourPosition"] = true
-          response.push(json);
-          idx ++;
-      })
+  const getData = async (parameter) => {
+    const snapshot = await getDocs(
+      query(donations, orderBy(parameter, "desc"), limit(3))
+    );
+    let response = [];
+    let idx = 1;
+    let json;
+    snapshot.forEach((doc) => {
+      console.log(doc.id, " => ", doc.data());
+      json = doc.data();
+      json["id"] = doc.id;
+      json["place"] = idx;
+      json["value"] = doc.data()[parameter];
+      response.push(json);
+      idx++;
+    });
+    const yourPosition = await getYourPosition(parameter);
+    console.log("YP L : ", yourPosition.length);
+
+    if (yourPosition.length > 0) {
+      response.push(yourPosition[0]);
     }
 
+    console.log(response.length);
+    setData(response);
+  };
 
-        return response;
-        // setYourPosition(response)
-
+  const getStats = async () => {
+    const Stats = collection(db, "Stats");
+    const stats = await getDoc(doc(Stats, "stats"));
+    let response = stats.exists() ? stats.data().total : 0;
+    setStats(response);
+  };
+  const getYourPosition = async (parameter) => {
+    const snapshot = await getDocs(
+      query(
+        donations,
+        where("user", "==", auth["currentUser"]["displayName"]),
+        limit(1)
+      )
+    );
+    let response = [];
+    let idx = 1;
+    let json;
+    if (!snapshot.empty) {
+      snapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.id, " => ", doc.data());
+        json = doc.data();
+        json["id"] = doc.id;
+        json["place"] = idx;
+        json["value"] = doc.data()[parameter];
+        json["isYourPosition"] = true;
+        response.push(json);
+        idx++;
+      });
+    } else {
+      await setDoc(doc(donations), {
+        user: auth["currentUser"]["displayName"],
+        referals: 0,
+        donations: 0,
+        total: 0,
+      });
+      const snapshot = await getDocs(
+        query(
+          donations,
+          where("user", "==", auth["currentUser"]["displayName"]),
+          limit(1)
+        )
+      );
+      snapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.id, " => ", doc.data());
+        json = doc.data();
+        json["id"] = doc.id;
+        json["place"] = idx;
+        json["value"] = doc.data()[parameter];
+        json["isYourPosition"] = true;
+        response.push(json);
+        idx++;
+      });
     }
 
-    React.useEffect(() => {
-        getData("total")
-        getStats()
-        // getYourPosition()
-    }, [])
-   
+    return response;
+    // setYourPosition(response)
+  };
+
+  React.useEffect(() => {
+    getData("donations");
+    getStats();
+    // getYourPosition()
+  }, []);
+
   const renderItem = ({ item }) => {
-   
-    return <Item place = {item.place} user = {item.user} value = {item.value} isYourPosition = {item.isYourPosition!= undefined ? item.isYourPosition : false}/>
-    
-  }
+    return (
+      <Item
+        place={item.place}
+        user={item.user}
+        value={item.value}
+        isYourPosition={
+          item.isYourPosition != undefined ? item.isYourPosition : false
+        }
+      />
+    );
+  };
   return (
     <View style={styles.container}>
       <View style={styles.top_container}>
-      <View style={{ marginTop: 20 }}>
-        <InformacionIconButton screen={"Details"} />
+        <View style={{ marginTop: 20 }}>
+          <InformacionIconButton screen={"Details"} />
+        </View>
       </View>
-    </View>
 
       <View style={styles.main_container}>
-          
         <Text style={styles.light_title_style}> Se parte de la ayuda! </Text>
-        <Text style={styles.detailText}>{`Ayuda a BAMX con su mision de terminar con el 
+        <Text
+          style={styles.detailText}
+        >{`Ayuda a BAMX con su mision de terminar con el 
 hambre en mexico. Dona y ve nuestro progreso!`}</Text>
-<View style = {{marginBottom : 15}}>
-  <Text style={styles.veryBigText}> ${stats} </Text>
-  </View>
+        <View style={{ marginBottom: 15 }}>
+          <Text style={styles.veryBigText}> ${stats} </Text>
+        </View>
 
-<View style = {styles.textContainer}>
-        </View> 
-        <View style = {styles.dashboard}><Dashboard item = {data != undefined ? data[0] : {}}/></View>
-        
+        <View style={styles.textContainer}></View>
+        <View style={styles.dashboard}>
+          <Dashboard item={data != undefined ? data[0] : {}} />
+        </View>
+
         <View style={styles.button_container_row}>
-          <TouchableOpacity style={styles.button_style_row_yellow} onPress = {()=>{getData("total")}}>
+          <TouchableOpacity
+            style={styles.button_style_row_yellow}
+            onPress={() => {
+              getData("donations");
+            }}
+          >
             <Text style={styles.button_text}>Donaciones</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.button_style_row_gray} onPress = {()=>{getData("referals")}}>
+          <TouchableOpacity
+            style={styles.button_style_row_gray}
+            onPress={() => {
+              getData("referals");
+            }}
+          >
             <Text style={styles.button_text}>Referidos</Text>
           </TouchableOpacity>
         </View>
-    <View style = {styles.leaderboard}>
-        <FlatList style = {{marginBottom: 10}}
-        data={data}
-        renderItem={renderItem}
-        keyExtractor={(item, index) => index + item.id + item.place}
-      />
+        <View style={styles.leaderboard}>
+          <FlatList
+            style={{ marginBottom: 10 }}
+            data={data}
+            renderItem={renderItem}
+            keyExtractor={(item, index) => index + item.id + item.place}
+          />
 
-        
-       {/* <You item = {yourPosition != undefined ? yourPosition[0] : {}}/> */}
-       </View>
+          {/* <You item = {yourPosition != undefined ? yourPosition[0] : {}}/> */}
+        </View>
 
-        <View style={styles.button_container} onTouchEnd={() =>
-          navigation.navigate('Donate')
-        }>
+        <View
+          style={styles.button_container}
+          onTouchEnd={() => navigation.navigate("Donate")}
+        >
           <TouchableOpacity style={styles.button_style}>
             <Text style={styles.button_text}>Donar</Text>
           </TouchableOpacity>
@@ -189,38 +244,37 @@ hambre en mexico. Dona y ve nuestro progreso!`}</Text>
 }
 
 const styles = StyleSheet.create({
-  dashboard : {
-    maxHeight: "25%"
+  dashboard: {
+    maxHeight: "25%",
   },
-  leaderboard:{
-    maxHeight: "30%"
-    
+  leaderboard: {
+    maxHeight: "30%",
   },
-    veryBigText: {
-        fontSize: 36,
-        color: "#FBC714",
-        fontWeight: "900"
-    },
-    row_with_margin: {
-      paddingRight: 10,
-      paddingLeft: 30,
-        marginBottom: 6
-    },
-    row_with_margin_and_line: {
-      paddingRight: 10,
-      paddingLeft : 30,
-        paddingBottom: 10,
-        borderBottomColor: "#3A3A3A",
-        borderBottomWidth: 2,
-        marginBottom: 12
-    },
+  veryBigText: {
+    fontSize: 36,
+    color: "#FBC714",
+    fontWeight: "900",
+  },
+  row_with_margin: {
+    paddingRight: 10,
+    paddingLeft: 30,
+    marginBottom: 6,
+  },
+  row_with_margin_and_line: {
+    paddingRight: 10,
+    paddingLeft: 30,
+    paddingBottom: 10,
+    borderBottomColor: "#3A3A3A",
+    borderBottomWidth: 2,
+    marginBottom: 12,
+  },
   container: {
     backgroundColor: palette.background,
     flex: 1,
-    paddingBottom:"45%",
+    paddingBottom: "45%",
   },
   textContainer: {
-      marginBottom: 30
+    marginBottom: 30,
   },
   top_container: {
     display: "flex",
@@ -230,7 +284,7 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     marginTop: 10,
     paddingHorizontal: 24,
-    paddingTop : 15,
+    paddingTop: 15,
   },
   tiny_logo: {
     alignSelf: "flex-end",
@@ -253,19 +307,19 @@ const styles = StyleSheet.create({
     display: "flex",
     alignItems: "center",
   },
-  detailText : {
-      paddingHorizontal: 36,
-      textAlign: 'justify',
-      fontSize: 13,
-      color : palette.textColor,
-      marginBottom: 7
+  detailText: {
+    paddingHorizontal: 36,
+    textAlign: "justify",
+    fontSize: 13,
+    color: palette.textColor,
+    marginBottom: 7,
   },
   light_title_style: {
     color: palette.textColor,
     fontSize: 16,
     fontWeight: "700",
     lineHeight: 36,
-    marginBottom : 7
+    marginBottom: 7,
   },
   title_style: {
     color: palette.blueText,
